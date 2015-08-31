cba99775628a219936fa60fdc7e0f97e268e7fd3
sebastianf <sebo_024@yahoo.com>
2015-08-31 23:14:12 +0200

    Added the initmap function to the viewModel

--- script.js
+++ script.js
@@ -5,27 +5,29 @@ var locations = [
 	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}]}
 ]
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 43.281775, lng: 12.074211},
-    zoom: 4
-  });
-
-  for(items in locations){
-	  var marker = new google.maps.Marker({
-	   	map: map,
-	    draggable: true,
-	    animation: google.maps.Animation.DROP,
-	    position: locations[items].coor[0]
-	  });
-  }
-}
 
 var myViewModel = function(){
 	this.listObservable = ko.observableArray();
+
 	locations.forEach(function(locItem){
 		this.listObservable.push(locItem);
 	});
+
+	this.initMap = function(){
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 43.281775, lng: 12.074211},
+    		zoom: 4
+		});
+
+		for(items in locations){
+		  var marker = new google.maps.Marker({
+		   	map: map,
+		    draggable: true,
+		    animation: google.maps.Animation.DROP,
+		    position: locations[items].coor[0]
+		  });
+  		}
+	}
 }
 
 ko.applyBindings(myViewModel);
