aa6bcc8c3c3190db008bc38fc92268eeac22418f
sebastianff <sebastianffarago@gmail.com>
2015-08-31 21:20:03 +0200

    Added a marker

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ function initMap() {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+
+  var marker = new google.maps.Marker({
+   	map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: {lat: -34.397, lng: 150.644}
+  });
 }
