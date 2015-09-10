e028b37a6c524787972e470b61abe885618188d3
sebastianf <sebo_024@yahoo.com>
2015-09-10 09:13:44 +0200

    Cleaned up the code, added an error message

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ var locations = [
 	{name:"United Kingdom",coor:[{lat:51.510795, lng:-0.105520}]},
 	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}]}
 ]
-var test = ["da","ne","mozda","hmm","da","sebo"]
 
-//
 var map;
-	//
 
 	initMap = function(){
 		map = new google.maps.Map(document.getElementById('map'), {
@@ -17,20 +14,17 @@ var map;
 		});
 	}
 
-
-
 var myViewModel = function(){
 
 	this.markers = ko.observableArray();
+
 	this.listObservable = ko.observableArray();
 
 	this.populateList = function(){
 		locations.forEach(function(locItem){
-					this.listObservable.push(locItem);
-			});
-
+			this.listObservable.push(locItem);
+		});
 	}
-
 
 	this.filterLocations = function(){
 		listObservable.removeAll();
@@ -43,22 +37,21 @@ var myViewModel = function(){
 	}
 
 	this.addMarkers = function(location,message){
-			  var marker = new google.maps.Marker({
-			    draggable: true,
-			    animation: google.maps.Animation.DROP,
-			    position: location
-			  });
-			  attachWin(marker,message);
-			markers().push(marker);
+	    var marker = new google.maps.Marker({
+	    	animation: google.maps.Animation.DROP,
+	    	position: location
+	  	});
+	    attachWin(marker,message);
+	    markers().push(marker);
   	}
 
   	this.createMarkers = function(){
-  		deleteMarkers();
+  		setMapOnAll(null);
+  		markers.removeAll();
   		for (items in listObservable()){
   			addMarkers(listObservable()[items].coor[0],listObservable()[items].name);
-  			//console.log(listObservable()[items].coor);
   		}
-  		showMarkers();
+  		setMapOnAll(map);
   	}
 
   	this.setMapOnAll = function(map){
@@ -67,39 +60,24 @@ var myViewModel = function(){
   		}
 	}
 
-	this.showMarkers =  function(){
-		setMapOnAll(map)
-	}
-
-	this.deleteMarkers =  function(){
-		setMapOnAll(null);
-		markers.removeAll();
-	}
-
 	this.attachWin = function(marker,message){
 		var infowindow = new google.maps.InfoWindow();
 
   		marker.addListener('click', function() {
-  			infowindow.close();
   			receiveData();
-  			setTimeout(function(){infowindow.setContent(da)},600);
+  			setTimeout(function(){infowindow.setContent(dataReceived)},600);
     		infowindow.open(marker.get('map'), marker);
 
   		});
 	}
 
-	this.getInfo = function(){
-		return("da")
+	this.receiveData = function(){
+		url = "http://api.nytimes.com/svc/search/v2/articlesearch.jssson?q=france&page=2&sort=oldest&api-key=231e8ef758feba7528cc7418f8c7e894:2:72283946";
+		$.getJSON(url,function(data){
+        	dataReceived = data.response.docs[0].headline.main;
+		}).error(function(){dataReceived="Request couldn't be completed"})
 	}
 
-	this.receiveData = function(){
-		url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=france&page=2&sort=oldest&api-key=231e8ef758feba7528cc7418f8c7e894:2:72283946";
-		$.getJSON(url,function(data){
-        	da = data.response.docs[0].headline.main;
-        	setTimeout(function(){return(da)},20000);
-        	//return(da);
-		})
-	}
   	this.populateList();
 
 }
