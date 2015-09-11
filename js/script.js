3b44682bf8aafc34438e1f5b89fcff661751389f
sebastianf <sebo_024@yahoo.com>
2015-09-11 18:48:34 +0200

    Added comments

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var locations = [
 	{name:"France",coor:[{lat:48.998754, lng:230460}]},
 	{name:"Italy",coor:[{lat:43.281775, lng:12.074211}]},
 	{name:"United Kingdom",coor:[{lat:51.510795, lng:-0.105520}]},
-	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}]}
+	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}]}//This is the list of all the locations we use.
 ]
 
 var map;
@@ -14,20 +14,20 @@ var map;
 		});
 	}
 
-var myViewModel = function(){
+var myViewModel = function(){//A viewModel used for knockout.js
 
-	this.markers = ko.observableArray();
+	this.markers = ko.observableArray();//This array is gonna hold the marker objects
 
-	this.listObservable = ko.observableArray();
+	this.listObservable = ko.observableArray();//This array will hold the data for the markers we are displaying
 
-	this.populateList = function(){
+	this.populateList = function(){//This function populates the inital list and ads the inistal markers to the map
 		locations.forEach(function(locItem){
 			this.listObservable.push(locItem);
 		});
 		setTimeout(function(){createMarkers()},1000);
 	}
 
-	this.filterLocations = function(){
+	this.filterLocations = function(){//This function filter the listobservable array based on user search queries
 		listObservable.removeAll();
 		locations.forEach(function(locItem){
 			var userText = document.getElementById('userInput').value;
@@ -35,10 +35,10 @@ var myViewModel = function(){
 					this.listObservable.push(locItem);
 				}
 		});
-		createMarkers();
+		createMarkers();//We call the create markers function to create the filtered markers
 	}
 
-	this.clickLocations = function(){
+	this.clickLocations = function(){//This function displays the clicker marker from the list
 		var clickedItem = this.name;
 		locations.forEach(function(locItem){
 				if (locItem.name.toLowerCase().indexOf(clickedItem.toLowerCase())==0){
@@ -50,7 +50,7 @@ var myViewModel = function(){
 		});
 	}
 
-	this.createMarkers = function(){
+	this.createMarkers = function(){//This fucntion is called from other functions to create the markers
   		setMapOnAll(null);
   		markers.removeAll();
   		for (items in listObservable()){
@@ -59,7 +59,7 @@ var myViewModel = function(){
   		setMapOnAll(map);
   	}
 
-	this.addMarkers = function(location,message){
+	this.addMarkers = function(location,message){//This fucntion created the basic marker to be used in other fucntions
 	    var marker = new google.maps.Marker({
 	    	animation: google.maps.Animation.DROP,
 	    	position: location
@@ -70,11 +70,11 @@ var myViewModel = function(){
 
   	this.setMapOnAll = function(map){
   		for (var i = 0; i < markers().length; i++) {
-    		markers()[i].setMap(map);
+    		markers()[i].setMap(map);//Set the map on markers to make them visible
   		}
 	}
 
-	this.attachWin = function(marker,message){
+	this.attachWin = function(marker,message){//Function used to add the infoWindow to the markers
 		var infowindow = new google.maps.InfoWindow();
 
   		marker.addListener('click', function() {
@@ -87,7 +87,7 @@ var myViewModel = function(){
   		});
 	}
 
-	this.receiveData = function(message){
+	this.receiveData = function(message){//This function makes the AJAX call for each marker when clicked
 		url = "http://api.population.io:80/1.0/population/"+message+"/2015-12-24/";
 		$.getJSON(url,function(data){
 			var sentence = "Population of " + message + " is ";
@@ -96,7 +96,7 @@ var myViewModel = function(){
 		}).error(function(){dataReceived="Request couldn't be completed"})
 	}
 
-  	this.populateList();
+  	this.populateList();//Call to the function to initaly populate the list
 
 }
 ko.applyBindings(myViewModel);
