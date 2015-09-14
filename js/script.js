1d2ecfe91819855ec31052a4e30c5d25dad318e4
sebastianf <sebo_024@yahoo.com>
2015-09-14 21:10:12 +0200

    Fixed the windows closing

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var locations = [
-	{name:"France",coor:[{lat:48.998754, lng:23.04600}],marker:""},
-	{name:"Italy",coor:[{lat:43.281775, lng:12.074211}],marker:""},
-	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}],marker:""},
-	{name:"Serbia",coor:[{lat:44.867888, lng:20.426739}],marker:""},
-	{name:"Spain",coor:[{lat:40.426406, lng:-3.699224}],marker:""},
-	{name:"Greece",coor:[{lat:37.919440, lng:23.708987}],marker:""}//This is the list of all the locations we use.
+	{name:"France",coor:[{lat:48.998754, lng:23.04600}]},
+	{name:"Italy",coor:[{lat:43.281775, lng:12.074211}]},
+	{name:"Belgium",coor:[{lat:50.515523, lng:4.816355}]},
+	{name:"Serbia",coor:[{lat:44.867888, lng:20.426739}]},
+	{name:"Spain",coor:[{lat:40.426406, lng:-3.699224}]},
+	{name:"Greece",coor:[{lat:37.919440, lng:23.708987}]}//This is the list of all the locations we use.
 ];
 
 var map;
@@ -24,11 +24,13 @@ var myViewModel = function(){//A viewModel used for knockout.js
 
 	this.userInput = ko.observable();
 
+	this.infowindows = ko.observableArray();
+
 	this.populateList = function(){//This function populates the inital list and ads the initial markers to the map
 		locations.forEach(function(locItem){
 			this.listObservable.push(locItem);
 		});
-		setTimeout(function(){createMarkers();},1000);
+		setTimeout(function(){createMarkers();infowindow = new google.maps.InfoWindow();},1000);
 	};
 
 	this.filterLocations = function(){//This function filter the listobservable array based on user search queries
@@ -55,7 +57,6 @@ var myViewModel = function(){//A viewModel used for knockout.js
 
 	this.createMarkers = function(){//This fucntion is called from other functions to create the markers
   		setMapOnAll(null);
-  		markers.removeAll();
   		for (var i=0;i<listObservable().length;i++){
   			addMarkers(listObservable()[i].coor[0],listObservable()[i].name);
   		}
@@ -80,11 +81,12 @@ var myViewModel = function(){//A viewModel used for knockout.js
 	};
 
 	this.attachWin = function(marker,message){//Function used to add the infoWindow to the markers
-		var infowindow = new google.maps.InfoWindow();
+		infowindow.close();
+		infowindow = new google.maps.InfoWindow();
 		receiveData(message);//send the location name for the AJAX request
 		setTimeout(function(){marker.setAnimation(null)},1400);
 		marker.setAnimation(google.maps.Animation.BOUNCE);//ads the animation when marker clicked
-		infowindow.setContent(dataReceived);
+		setTimeout(function(){infowindow.setContent(dataReceived)},300);
 		infowindow.open(marker.get('map'), marker);
 		console.log(marker)
 	};
